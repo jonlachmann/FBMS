96578c363fa7c9e0ecc9d9db3dbcc768a9d0b0f5
aliaksah <math.uio.no>
2025-09-30 15:43:32 +0200

    new stuff on the functionality added to the example

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -4,47 +4,61 @@
 # Created on: 2021-02-24
 
 
-#' Plot convergence of best/median/mean/other summary log posteriors in time 
+#' Plot Convergence Diagnostics for GMJMCMC or GMJMCMC Merged Results
 #'
-#' @param res Object corresponding gmjmcmc output
-#' @param FUN The summary statistics to check convergence
-#' @param conf Which confidence intervals to plot
-#' @param burnin How many first populations to skip
-#' @param window Sliding window for computing the standard deviation
-#' @param ylim Limits for the plotting range; if unspecified, min and max of confidence intervals will be used
-#' @param ... Additional graphical parameters passed to plot and lines functions, e.g. col, lwd, lty, main, xlab, ylab, ylim
+#' Plots the convergence of summary statistics (e.g., median, mean) of log posteriors or marginal likelihoods
+#' over populations for a GMJMCMC or GMJMCMC merged result object, with confidence intervals.
 #'
-#' @return A list of summary statistics for checking convergence with given confidence intervals
-#' 
+#' @param res Object of class \code{gmjmcmc} or \code{gmjmcmc_merged} containing results from a GMJMCMC run or merged runs.
+#' @param FUN Function to compute summary statistics (e.g., \code{median}, \code{mean}). Default is \code{median}.
+#' @param conf Numeric; confidence level for intervals (e.g., 0.95 for 95%). Default is 0.95.
+#' @param burnin Integer; number of initial populations to skip. Default is 0.
+#' @param window Integer; size of the sliding window for computing standard deviation. Default is 5.
+#' @param ylim Numeric vector; y-axis limits for the plot. If \code{NULL}, computed from confidence intervals.
+#' @param ... Additional graphical parameters passed to \code{plot} and \code{lines} (e.g., \code{col}, \code{lwd}, \code{lty}, \code{main}, \code{xlab}, \code{ylab}).
+#'
+#' @return Returns \code{invisible(NULL)}. The function is called for its side effect of producing a plot.
+#'
 #' @examples
-#' result <- gmjmcmc(y = matrix(rnorm(100), 100),
-#'                   x = matrix(rnorm(600), 100), 
-#'                   P = 2, 
-#'                   transforms =  c("p0", "exp_dbl"))
-#' diagnstats <- diagn_plot(result)
-#' 
+#' data(exoplanet)
+#' result <- fbms(semimajoraxis ~ ., data = exoplanet, method = "gmjmcmc", transforms = c("sin"))
+#' diagn_plot(result, FUN = median, conf = 0.95, main = "Convergence Plot")
+#'
 #' @export
 diagn_plot <- function(res, FUN = median, conf = 0.95, burnin = 0, window = 5, ylim = NULL, ...) {
+  # Input validation
+  stopifnot(
+    "res must be of class 'gmjmcmc' or 'gmjmcmc_merged'" = inherits(res, c("gmjmcmc", "gmjmcmc_merged")),
+    "res must contain best.log.posteriors or best.margs" = length(res$thread.best) > 0 || length(res$best.margs) > 0,
+    "FUN must be a function" = is.function(FUN),
+    "conf must be between 0 and 1" = is.numeric(conf) && conf > 0 && conf < 1,
+    "burnin must be a non-negative integer" = is.numeric(burnin) && burnin >= 0 && burnin %% 1 == 0,
+    "window must be a positive integer" = is.numeric(window) && window >= 1 && window %% 1 == 0
+  )
   
   args <- list(...)
   args[["..."]] <- NULL  # Remove any "..." element to avoid warning
   
-  if(length(res$thread.best) > 0)
+  # Extract matrix.results
+  if (length(res$thread.best) > 0) {
     matrix.results <- res$best.log.posteriors
-  else
+  } else {
     matrix.results <- as.matrix(unlist(res$best.margs))
+  }
+  stopifnot("matrix.results must have at least burnin + 1 rows" = nrow(matrix.results) > burnin)
   
-  sr <- sapply((1 + burnin):dim(matrix.results)[1], function(x) FUN(matrix.results[x, ]))
+  # Compute summary statistics and confidence intervals
+  sr <- sapply((burnin + 1):nrow(matrix.results), function(x) FUN(matrix.results[x, ]))
   sds <- c(0, sapply(2:length(sr), function(x) sd(sr[max(1, x - window):x])))
-  
   ub <- sr + qnorm(1 - (1 - conf) / 2) * sds
   lb <- sr - qnorm(1 - (1 - conf) / 2) * sds
   
-  if (is.null(ylim) && is.null(args$ylim))
+  # Set default plot parameters
+  if (is.null(ylim) && is.null(args$ylim)) {
     ylim <- c(min(lb), max(ub))
-  else if (is.null(ylim))
+  } else if (is.null(ylim)) {
     ylim <- args$ylim
-  
+  }
   main <- if (!is.null(args$main)) args$main else "Convergence"
   xlab <- if (!is.null(args$xlab)) args$xlab else "Population"
   ylab <- if (!is.null(args$ylab)) args$ylab else "Summary"
@@ -54,10 +68,11 @@ diagn_plot <- function(res, FUN = median, conf = 0.95, burnin = 0, window = 5, y
   args$ylab <- NULL
   args$ylim <- NULL
   
+  # Create plot
   do.call(plot, c(
     list(
       y = sr,
-      x = (burnin + 1):(dim(matrix.results)[1]),
+      x = (burnin + 1):nrow(matrix.results),
       type = "l",
       col = 1,
       ylim = ylim,
@@ -67,27 +82,26 @@ diagn_plot <- function(res, FUN = median, conf = 0.95, burnin = 0, window = 5, y
     ),
     args
   ))
-  
   do.call(lines, c(
     list(
       y = ub,
-      x = (burnin + 1):(dim(matrix.results)[1]),
+      x = (burnin + 1):nrow(matrix.results),
       col = 1,
       lty = 2
     ),
     args
   ))
-  
   do.call(lines, c(
     list(
       y = lb,
-      x = (burnin + 1):(dim(matrix.results)[1]),
+      x = (burnin + 1):nrow(matrix.results),
       col = 1,
       lty = 2
     ),
     args
   ))
   
-  return(list(stat = sr, lower = lb, upper = ub))
+  invisible(NULL)
 }
+
 
--- tests_current/Ex1_Sec3.R
+++ tests_current/Ex1_Sec3.R
@@ -34,7 +34,7 @@ set.seed(123)
 
 result.default <- fbms(formula = semimajoraxis ~ 1 + . , data = df.train, method = "gmjmcmc", transforms = transforms)
 
-
+get.best.model(result.default)
 
 ####################################################
 #
@@ -58,7 +58,7 @@ result.P50 <- fbms(data = df.train, method = "gmjmcmc", transforms = transforms,
 set.seed(123)
 
 result_parallel <- fbms(data = df.train, method = "gmjmcmc.parallel", transforms = transforms,
-                          runs = 40, cores = 40, P = 25)
+                          runs = 40, cores = 8, P = 25)
 
 
 ####################################################
@@ -78,7 +78,7 @@ summary(result.P50)
 summary(result.P50, pop = "best", labels = paste0("x",1:length(df.train[,-1])))
 summary(result.P50, pop = "last", labels = paste0("x",1:length(df.train[,-1])))
 summary(result.P50, pop = "last", tol = 0.01, labels = paste0("x",1:length(df.train[,-1])))
-summary(result.P50, pop = "all")
+
 
 summary(result_parallel)
 library(tictoc)
@@ -121,13 +121,13 @@ plot(result_parallel)
 
 #preds <- predict(result.default, df.test[,-1], link = function(x) x)
 preds <-  predict(result.default, df.test[,-1])
-rmse.default <- sqrt(mean((preds$aggr$mean - df.test$semimajoraxis)^2))
+rmse.default <- sqrt(mean((mean(preds) - df.test$semimajoraxis)^2))
 
 pdf("prediction.pdf") 
-plot(preds$aggr$mean, df.test$semimajoraxis)
+plot(mean(preds), df.test$semimajoraxis)
 dev.off()
 
-plot(preds$aggr$mean, df.test$semimajoraxis)
+plot(mean(preds), df.test$semimajoraxis)
 
 
 
@@ -139,13 +139,13 @@ plot(preds$aggr$mean, df.test$semimajoraxis)
 
 #preds.P50 = predict(result.P50, df.test[,-1], link = function(x) x)  
 preds.P50 = predict(result.P50, df.test[,-1])  
-rmse.P50 <-  sqrt(mean((preds.P50$aggr$mean - df.test$semimajoraxis)^2))
+rmse.P50 <-  sqrt(mean((mean(preds.P50) - df.test$semimajoraxis)^2))
 
 pdf("prediction.P50.pdf") 
-plot(preds.P50$aggr$mean, df.test$semimajoraxis)
+plot(mean(preds.P50), df.test$semimajoraxis)
 dev.off()
 
-plot(preds.P50$aggr$mean, df.test$semimajoraxis)
+plot(mean(preds.P50), df.test$semimajoraxis)
 
 
 
@@ -153,10 +153,10 @@ plot(preds.P50$aggr$mean, df.test$semimajoraxis)
 
 
 preds.multi <- predict(result_parallel , df.test[,-1], link = function(x) x)
-rmse.parallel <- sqrt(mean((preds.multi$aggr$mean - df.test$semimajoraxis)^2))
+rmse.parallel <- sqrt(mean((mean(preds.multi) - df.test$semimajoraxis)^2))
 
 pdf("pred_parallel.pdf") 
-plot(preds.multi$aggr$mean, df.test$semimajoraxis)
+plot(mean(preds.multi), df.test$semimajoraxis)
 dev.off()
 
 
